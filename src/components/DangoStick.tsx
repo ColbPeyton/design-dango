7af8da2b81a9ba79ecaeebc212fdf12c2c5543a7
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-04-29 23:49:25 -0500

    create DangoStick file. add basic setup, CurrentDangoContext, and state to file

---
+++ src/components/DangoStick.tsx
@@ -0,0 +1,16 @@
+import React, {useContext} from 'react';
+import {CurrentDangoContext} from '../Contexts/CurrentDango';
+
+
+interface DangoStickState{
+
+}
+
+
+export const DangoStick = ():JSX.Element => {
+    return(
+        <div className='container-dango-stick'>
+
+        </div>
+    )
+}
