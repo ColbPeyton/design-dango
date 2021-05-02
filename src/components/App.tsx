9622e321199afd1b6f9691eef129a01b14489673
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-05-02 15:40:07 -0500

    create App styling and add basic styling. add container styling to index

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {CurrentDangoProvider} from '../Contexts/CurrentDango';
 import {DangoSelection} from './DangoSelection';
 import {DangoStick} from './DangoStick';
+import '../styles/App.scss';
 
 export const App = ():JSX.Element => {
   return (
-    <div>
+    <div className='container-app'>
       <CurrentDangoProvider>
         <DangoStick />
         <DangoSelection />
