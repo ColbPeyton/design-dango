e0120cf7db9ad829224c06cd7131c973ae5c6372
Colby Peyton <colby.peyton@gmail.com>
2021-05-04 15:44:02 -0500

    add context to Header. implement resetDango context method on reset button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React,{useContext} from 'react';
+import {CurrentDangoContext, defaultDango} from '../Contexts/CurrentDango';
 import '../styles/Header.scss';
 
+interface DangoStickProps{
+    currentDango: defaultDango[];
+    addDango: (id:defaultDango)=> void;
+    removeDango: (id:number)=> void;
+    isLocatedInDango: (id:number)=> boolean;
+    resetDango: () => void;
+}
+
+
 export const Header  = ():JSX.Element => {
+
+    const context = useContext<DangoStickProps>(CurrentDangoContext);
+
     return(
         <div className='container-header'>
             <div className='contatiner-top-btn'>
-                <button className='btn'>
+                <button className='btn' onClick={()=> context.resetDango()}>
                     reset
                 </button>
             </div>
