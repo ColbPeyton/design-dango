ddd5b4d95cd8f45be83921d6842afc7114c45db9
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-05-05 17:32:43 -0500

    update footer Dango, DangoSelection styling. update footer markup

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = ():JSX.Element => {
     return(
         <div className='container-footer'>
             <div className='top'>
-                <p>I like Monster Hunter. Please don't sue me</p>
+                <p>I like Monster Hunter.</p> <p>Please don't sue me.</p>
             </div>
             <div className='bot'>
                 <p>Developed by <a href={link}>ColbPeyton</a></p>
