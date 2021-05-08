65f92587b46c5f3643028fab06581391c10d223c
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2021-05-07 23:03:01 -0500

    add mobile styling to EffectAlert, fix warning in Dango styling

--- src/components/EffectAlert.tsx
+++ src/components/EffectAlert.tsx
@@ -1,6 +1,7 @@
 import React,{useContext} from 'react';
 import {defaultContext, CurrentDangoContext} from '../Contexts/CurrentDango';
 import {randomKey} from '../_helpers/randomKey';
+import '../styles/EffectAlert.scss';
 
 export const EffectAlert = ():JSX.Element => {
     const context = useContext<defaultContext>(CurrentDangoContext);
@@ -21,9 +22,12 @@ export const EffectAlert = ():JSX.Element => {
 
             }
             return(
-                <div className='container-effect'>
-                    <h3>Nothing this time...</h3>
+                <div className='list'>
+                    <div className='container-effect'>
+                        <h3>Nothing this time...</h3>
+                    </div>
                 </div>
+                
             )
         }
         return <div></div>
@@ -32,10 +36,15 @@ export const EffectAlert = ():JSX.Element => {
     const effectDisplay = ():JSX.Element => {
         return(
             <div className='container-effect-alert'>
+                <div className='container-title'>
+                    <h2>Dango Skills activated!</h2>
+                </div>
                 {renderList()}
-                <button className='btn' onClick={()=> context.resetDangoContext()}>
-                    Try Again? 
-                </button>
+                <div className='container-btn'>
+                    <button className='btn' onClick={()=> context.resetDangoContext()}>
+                        Try Again? 
+                    </button>
+                </div>
             </div>
         )
     }
